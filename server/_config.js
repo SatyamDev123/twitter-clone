dc1bbea4df3160c9a835ed0c6c02440c00d09a28
satyam dev <satyamdev123@gmail.com>
2017-07-25 21:38:13 +0530

    updated config

--- server/_config.js
+++ server/_config.js
@@ -1,7 +1,7 @@
 var config = {
   twitter: {
-    consumerKey: 'u5DCdOHQNprm8hVrRmN6KG5ow',
-    consumerSecret: 'MB2cKMmrL4JyzwNA3w13SCByMpygcWwMgwUFUwmH904t9GpEIZ',
+    consumerKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    consumerSecret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
   }
 };
